16eb4e9bb810b08170965e4218515f6152963281
Jean-Michel Fayard <jmfayard@gmail.com>
2025-01-14 14:24:38 +0100

    Streams sorted(comparing()||signum())

--- src/test/java/modernjava/StreamsJava8.java
+++ src/test/java/modernjava/StreamsJava8.java
@@ -1,10 +1,14 @@
 package modernjava;
 
-import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.lang.Integer.signum;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StreamsJava8 {
 
@@ -22,5 +26,31 @@ public class StreamsJava8 {
         String joined = words
                 .collect(Collectors.joining("', '", "['", "']"));
         assertThat(joined).isEqualTo("['un', 'deux', 'trois']");
+    }
+
+    @Test
+    void sortedComparing() {
+
+        var oldest1 = persons().sorted(Comparator.comparing(Person::age))
+                .findFirst().get();
+        var oldest2 = persons().sorted(Comparator.comparingInt(Person::age))
+                .findFirst().get();
+        var oldest3 = persons()
+                .sorted((a, b) -> signum(a.age-b.age))
+                .findFirst().get();
+        assertThat(oldest1.name).isEqualTo("MC");
+        assertThat(oldest2.name).isEqualTo("MC");
+        assertThat(oldest3.name).isEqualTo("MC");
+    }
+
+    record Person(int age, String name) {
+
+    }
+    Stream<Person> persons() {
+        return Stream.of(
+                new Person(42, "JM"),
+                new Person(40, "patrick"),
+                new Person(35, "MC")
+        );
     }
 }
